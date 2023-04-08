a3327c519413a52b93a0bb2a678bf31b75dce97e
mina-v <mina.bessada@vodafone.com>
2023-04-08 07:16:04 +0200

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { ConfirmationComponent } from './confirmation/confirmation.component';
+import { ProductItemDetailComponent } from './product-item-detail/product-item-detail.component';
+import { ProductItemComponent } from './product-item/product-item.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    ConfirmationComponent,
+    ProductItemDetailComponent,
+    ProductItemComponent,
+    ProductListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
